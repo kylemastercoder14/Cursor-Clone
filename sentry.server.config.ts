27bff1f50a475ab8df78ad4927a52201e07706b2
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2026-01-09 10:30:02 +0800

    06: error tracking

---
+++ sentry.server.config.ts
@@ -0,0 +1,23 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://b0aabd290c22c493f903aee82779c311@o4510369682489344.ingest.us.sentry.io/4510677823586304",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+  integrations: [
+    Sentry.vercelAIIntegration,
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],
+});
